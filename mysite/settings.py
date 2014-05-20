f8b849b04cf5fae23167edb531e4549a7497fb14
PP <prakhar126@gmail.com>
2014-05-20 23:26:53 +0530

    trying to deploy

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,7 @@ DEBUG = True
 
 TEMPLATE_DEBUG = True
 
-ALLOWED_HOSTS = []
+
 
 TEMPLATE_DIRS=(
    os.path.join(BASE_DIR,'bookstore/templates'),
@@ -113,3 +113,24 @@ EMAIL_HOST_USER = 'prakhar126'
 EMAIL_HOST_PASSWORD = ''
 DEFAULT_FROM_EMAIL = 'prakhar126@gmail.com'
 DEFAULT_TO_EMAIL = 'to email'
+
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+"""import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)"""
