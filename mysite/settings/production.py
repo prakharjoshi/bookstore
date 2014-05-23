12fc6763e1a201663ac2cbd55a8544308cffebb1
PP <prakhar126@gmail.com>
2014-05-23 20:00:19 +0530

    run

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -21,7 +21,7 @@ def get_env_setting(setting):
 
 ########## HOST CONFIGURATION
 # See: https://docs.djangoproject.com/en/1.5/releases/1.5/#allowed-hosts-required-in-production
-ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = []
 ########## END HOST CONFIGURATION
 
 ########## EMAIL CONFIGURATION
@@ -66,3 +66,23 @@ CACHES = {}
 SECRET_KEY = get_env_setting('SECRET_KEY')
 
 ########## END SECRET CONFIGURATION
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
