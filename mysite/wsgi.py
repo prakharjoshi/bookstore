48c7e06556c69ea78718f126765ab65905d7b65f
PP <prakhar126@gmail.com>
2014-05-23 22:11:15 +0530

    o.o

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -12,6 +12,6 @@ from django.core.wsgi import get_wsgi_application
 #from dj_static import Cling 
 
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings.production")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 application = get_wsgi_application()
 #application = Cling(get_wsgi_application())
