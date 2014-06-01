b44e5ffb189b7246e3fcff8be4bb279b5a60b11a
prakhar <prakhar126@gmail.com>
2014-06-01 10:55:45 +0530

    Update production.py

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -1,11 +1,5 @@
 """Production settings and globals."""
 
-#from __future__ import absolute_import
-
-#import os
-#import sys
-
-#from os import environ
 import os
 from .base import *
 
@@ -14,64 +8,7 @@ from .base import *
 #from django.core.exceptions import ImproperlyConfigured
 
 
-"""def get_env_setting(setting):
-        Get the environment setting or return exception
-    try:
-        return environ[setting]
-    except KeyError:
-        error_msg = "Set the %s env variable" % setting
-        raise ImproperlyConfigured(error_msg)"""
-
-########## HOST CONFIGURATION
-# See: https://docs.djangoproject.com/en/1.5/releases/1.5/#allowed-hosts-required-in-production
-#ALLOWED_HOSTS = []
-########## END HOST CONFIGURATION
-
-"""########## EMAIL CONFIGURATION
-# See: https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-
-# See: https://docs.djangoproject.com/en/dev/ref/settings/#email-host
-EMAIL_HOST = environ.get('EMAIL_HOST', 'smtp.gmail.com')
-
-# See: https://docs.djangoproject.com/en/dev/ref/settings/#email-host-password
-EMAIL_HOST_PASSWORD = environ.get('EMAIL_HOST_PASSWORD', '')
-
-# See: https://docs.djangoproject.com/en/dev/ref/settings/#email-host-user
-EMAIL_HOST_USER = environ.get('EMAIL_HOST_USER', 'prakhar126@gmail.com')
-
-# See: https://docs.djangoproject.com/en/dev/ref/settings/#email-port
-EMAIL_PORT = environ.get('EMAIL_PORT', 587)
-
-# See: https://docs.djangoproject.com/en/dev/ref/settings/#email-subject-prefix
-EMAIL_SUBJECT_PREFIX = '[%s] ' % mysite
-
-# See: https://docs.djangoproject.com/en/dev/ref/settings/#email-use-tls
-EMAIL_USE_TLS = True
-
-# See: https://docs.djangoproject.com/en/dev/ref/settings/#server-email
-SERVER_EMAIL = EMAIL_HOST_USER
-########## END EMAIL CONFIGURATION"""
 DEBUG = False
-
-#TEMPLATE_DEBUG = DEBUG
-########## DATABASE CONFIGURATION
-#DATABASES = {}
-########## END DATABASE CONFIGURATION
-
-
-########## CACHE CONFIGURATION
-# See: https://docs.djangoproject.com/en/dev/ref/settings/#caches
-#CACHES = {}
-########## END CACHE CONFIGURATION
-
-
-########## SECRET CONFIGURATION
-# See: https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
-#SECRET_KEY = get_env_setting('SECRET_KEY')
-#SECRET_KEY = 'SECRET_KEY'
-
-########## END SECRET CONFIGURATION
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
@@ -84,30 +21,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 
 # Static asset configuration
-#import os
-"""BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)"""
-
-
-"""PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
-#STATIC_ROOT = "/var/www/example.com/static/"
-#os.path.join(PROJECT_PATH, 'staticfiles')
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_PATH, 'static'),
-)
-
-STATICFILES_FINDERS=(
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-)"""
 
 import os
 PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
@@ -116,5 +30,4 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(PROJECT_PATH, 'static'),
-    #os.path.join(BASE_DIR,'static'),
 )
