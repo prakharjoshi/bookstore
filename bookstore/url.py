db8561fcc2b63bd18865434ba27c5aabf39d2131
PP <prakhar126@gmail.com>
2014-06-23 21:43:31 +0530

    authentication templates 1

--- bookstore/url.py
+++ bookstore/url.py
@@ -24,8 +24,7 @@ urlpatterns = patterns(' ',
 	url(r'^contact_us$',views.contact_us,name='contact_us'),
 	url(r'^support$',views.support,name='support'),
 	url(r'^privacy_policy$',views.private_policy,name='private_policy'),
-	#url(r'^password_change/$',views.password_change,name='password_change'),
-	#url(r'^password_change_done/$',views.password_change_done,name='password_change_done'),
+	
 	
 )  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
